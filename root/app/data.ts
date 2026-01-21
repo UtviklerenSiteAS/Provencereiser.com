588514180a45f79e174f0f70b605790cf496e1cc
preztel <eliaselvaa@gmail.com>
2026-01-21 17:25:29 +0100

    Init

---
+++ root/app/data.ts
@@ -0,0 +1,77 @@
+
+export const courses = [
+  {
+    title: "Mat- og vinkurs",
+    norwegian: "Med Trond Moi",
+    description: "Opplev ekte matglede rundt langbordet. Trond Moi leder oss gjennom kulinariske høydepunkter og viner vi setter pris på.",
+    image: "/images/Provencereiser/58659978_10156064215367127_2925486423568023552_n.jpeg",
+  },
+  {
+    title: "Trøffeljakt",
+    norwegian: "Med Trøffellauget",
+    description: "Sammen med trøffellauget ønsker vi velkommen til jakt på skogens sorte gull i de små alper.",
+    image: "/images/Provencereiser/PROVENCE00003.jpeg",
+  },
+  {
+    title: "Malekurs",
+    norwegian: "Med Jan-Ove Tuv",
+    description: "Vi dykker inn i det klassiske figurative under god veiledning av Jan-Ove Tuv, som kaller seg for kitchmaler. Tidligere elev hos Odd Nerdrum.",
+    image: "/images/Malekurs_jan_ove_tuv.jpg",
+  },
+  {
+    title: "Malekurs",
+    norwegian: "Med Tore Wilhelm Johansen",
+    description: "Finn din kreative nerve og fargeglede sammen med Tore Wilhelm Johansen i inspirerende omgivelser.",
+    image: "/images/Malekurs_tore.jpg",
+  },
+  {
+    title: "Malekurs",
+    norwegian: "Med Jonny Andvik",
+    description: "Lær teknikker og få ny inspirasjon fra en av våre mest anerkjente malere, Jonny Andvik. Tidligere elev hos Odd Nerdrum.",
+    image: "/images/Malekurs_jonny_andvik.jpeg",
+  },
+  {
+    title: "Malekurs",
+    norwegian: "Med Billy Roy Økland",
+    description: "Billy Roy Økland er en klassisk figurative maler med internasjonal erfaring. Tidligere elev hos Odd Nerdrum.",
+    image: "/images/Malekurs_billy_roy_okland.jpg",
+  }
+];
+
+export const carouselImages = [
+  "/images/Bildekarusell/14123421_10157342832605099_1616591518_o.jpeg",
+  "/images/Bildekarusell/14329226_10157367601030099_950448890_o.jpeg",
+  "/images/Bildekarusell/14606445_1217870914935982_7216211984746739750_n.jpeg",
+  "/images/Bildekarusell/247307513_10209510575356815_3813937429691584066_n.jpeg",
+  "/images/Bildekarusell/309659383_3656179777947864_9168231354010006269_n.jpeg",
+  "/images/Bildekarusell/312422350_10220885772406692_7536706860180899450_n.jpeg",
+  "/images/Bildekarusell/35243337_1916494895049136_3730071877012422656_n.jpeg",
+  "/images/Bildekarusell/475983555_3950050005252420_4125721399273511225_n.jpeg",
+  "/images/Bildekarusell/571273839_25476142691992864_6370117515935918333_n.jpeg",
+  "/images/Bildekarusell/571306648_25476535351953598_4415130669478907049_n.jpeg",
+  "/images/Bildekarusell/58659978_10156064215367127_2925486423568023552_n.jpeg"
+];
+
+export const teamMembers = [
+  {
+    name: "Trond Moi",
+    role: "Mat- og vinkurs",
+    desc: "Kjent fra Bølgen & Moi. En mester på smaker som leder oss gjennom kulinariske høydepunkter."
+  },
+  {
+    name: "Jan-Ove Tuv",
+    role: "Malekurs",
+    desc: "Klassisk figurativ maler og tidligere elev av Odd Nerdrum. En inspirerende og kunnskapsrik veileder."
+  },
+  {
+    name: "Tore Wilhelm Johansen",
+    role: "Malekurs",
+    desc: "Anerkjent kunstner som hjelper deg å finne din kreative nerve og fargeglede i Provence."
+  },
+  {
+    name: "Jonny Andvik",
+    role: "Malekurs",
+    desc: "En av våre fremste figurative malere, kjent for sitt unike uttrykk og tekniske dyktighet."
+  }
+];
+
