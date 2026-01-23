c370baacb3df19469641808612d9ff413ba131f8
Elias Aarflot Elvsaas <60546504+preztel@users.noreply.github.com>
2026-01-23 13:59:26 +0100

    Change image source from PROVENCE00037 to Sissa

--- root/app/HomeClient.tsx
+++ root/app/HomeClient.tsx
@@ -213,7 +213,7 @@ export default function HomeClient({ carouselImages }: { carouselImages: string[
                             </div>
                             <div className="relative aspect-square md:aspect-[4/3] bg-stone-50 overflow-hidden shadow-lg">
                                 <Image
-                                    src="/images/PROVENCE00037.jpeg"
+                                    src="/images/Sissa.jpg"
                                     alt="Sissa Aabel"
                                     fill
                                     className="object-cover object-center opacity-90 sepia-[.1]"
